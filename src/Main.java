223848ded9ef185f8429394b1ee3e3973168c15a
SUYAMBULINGAMM <suyambulingam195@gmail.com>
2026-09-17 06:39:34 +0530

    threads

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
-package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws InterruptedException{
+        StringBuffer sb =new StringBuffer();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //thread 1
+        Thread t1=new Thread(()->{
+            for(int i=0;i<5;i++)
+            {
+                sb.append("A");
+            }
+        });
+        
+        Thread t2=new Thread(()->{
+            for(int i=0;i<5;i++)
+            {
+                sb.append("B");
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+        System.out.print(sb);
     }
 }
